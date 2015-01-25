LGSInnovations/sigplot

commit 15380dd4c0e7657950b23ef4b64d4488837b9e7e

While tracking xpos,ypos also keep a reference to mouse x,y in screen coordinates

js/mx.js
@@ -261,6 +261,10 @@ window.mx = window.mx || {};
         Mx.onmousemove = (function(Mx) {
             return function(e) {
                 var rect = e.target.getBoundingClientRect();
+                // Screen x/y of mouse
+                Mx.x = event.x || event.clientX;
+                Mx.y = event.y || event.clientY;
+                // Plot relative x/y of mouse
                 Mx.xpos = (e.offsetX === undefined) ? (e.pageX - rect.left - window.scrollX) : e.offsetX;
                 Mx.ypos = (e.offsetX === undefined) ? (e.pageY - rect.top - window.scrollY) : e.offsetY;
 

commit 085f1e89e45cb850c56e9543490921ba0fec2d31

Fix regression introduced by 15380dd

js/mx.js
@@ -267,8 +267,8 @@ window.mx = window.mx || {};
             return function(e) {
                 var rect = e.target.getBoundingClientRect();
                 // Screen x/y of mouse
-                Mx.x = event.x || event.clientX;
-                Mx.y = event.y || event.clientY;
+                Mx.x = e.x || e.clientX;
+                Mx.y = e.y || e.clientY;
                 // Plot relative x/y of mouse
                 Mx.xpos = (e.offsetX === undefined) ? (e.pageX - rect.left - window.scrollX) : e.offsetX;
                 Mx.ypos = (e.offsetX === undefined) ? (e.pageY - rect.top - window.scrollY) : e.offsetY;