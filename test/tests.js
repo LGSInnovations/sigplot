01378d0926f206406cdfd822f6c00ffd5bf4c01b
Michael Ihde <michael.ihde@axiosengineering.com>
2015-05-18 21:52:11 +0100

    Add custom symbol test for complex data

--- test/tests.js
+++ test/tests.js
@@ -1162,6 +1162,43 @@ interactiveTest('sigplot custom symbol', 'Do you see custom symbols?', function(
     });
 });
 
+interactiveTest('sigplot custom symbol complex', 'Do you see custom symbols in RGB order?', function() {
+    var container = document.getElementById('plot');
+    var plot = new sigplot.Plot(container, {
+        cmode: "RI"
+    });
+    notEqual(plot, null);
+
+    function custom_symbol(ctx, i, x, y) {
+        var n = (i % 3);
+        if (n === 0) {
+            ctx.strokeStyle = "red";
+            ctx.fillStyle = "red";
+        } else if (n === 1) {
+            ctx.strokeStyle = "green";
+            ctx.fillStyle = "green";
+        } else if (n === 2) {
+            ctx.strokeStyle = "blue";
+            ctx.fillStyle = "blue";
+        }
+        ctx.beginPath();
+        ctx.arc(x, y, 2, 0, 360);
+        ctx.fill();
+        ctx.stroke();
+    }
+
+    // make it so the line is RRRGGGBBB
+    var ramp = [1, 1, 4, 4, 7, 7, 2, 2, 5, 5, 8, 8, 3, 3, 6, 6, 9, 9];
+
+    plot.overlay_array(ramp, {
+        format: "CF"
+    }, {
+        name: "x",
+        symbol: custom_symbol,
+        line: 0
+    });
+});
+
 interactiveTest('sigplot custom symbol-line', 'Do you see custom symbols?', function() {
     var container = document.getElementById('plot');
     var plot = new sigplot.Plot(container, {});
