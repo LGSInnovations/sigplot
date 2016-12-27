42012d92e75a1263fdf305ba4cce1e6f55478c75
Michael Ihde <mihde@lgsinnovations.com>
2016-12-27 17:41:48 -0500

    Silently tolerate _onpipewrite calls when the internal buffer has not yet been created

--- js/sigplot.layer2d.js
+++ js/sigplot.layer2d.js
@@ -200,6 +200,11 @@
                 throw "Invalid draw mode";
             }
 
+	    if (!this.buf) {
+	       // the layer isn't setup correctly yet
+	       return;
+	    }
+
             // grab one row worth of data
             var ngot = m.grabx(this.hcb, this.buf, this.hcb.subsize * this.hcb.spa);
             if (ngot === 0) { // shouldn't happen because of the pavail check
