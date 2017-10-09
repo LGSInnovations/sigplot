9fa52d996b2a3a5f9ec039f655fe44b0d17a6fa0
Unknown <deseanjunk@yahoo.com>
2017-10-09 13:37:27 -0500

    Add Support for dom based menu

--- js/sigplot.js
+++ js/sigplot.js
@@ -5668,7 +5668,7 @@
 
         var REFRESH_ITEM = {
             text: "Refresh" // no handler, just let the finalizer deal with
-                // it
+            // it
         };
 
         var KEYPRESSINFO_ITEM = {
@@ -6284,7 +6284,9 @@
         if (!o.ylab) {
             o.ylab = 0;
         }
-
+        if (o.useDomMenu) {
+            Mx.useDomMenu = true;
+        }
         //Convert xunits and yunits to numbers if they are strings
         o.xlab = m.unit_lookup(o.xlab);
         o.ylab = m.unit_lookup(o.ylab);
