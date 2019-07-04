LGSInnovations/sigplot

commit ef66bc51347408191696c3daf683681d7e953485

added colorbar to legend

js/sigplot.js
@@ -5871,6 +5871,11 @@
                     mx.draw_symbol(Mx, ic, ix + tw - labelOffset, iy - 3,
                         Gx.lyr[n].symbol, thk);
                 }
+                if (Gx.lyr[n].hcb["class"] === 2) {
+                    //draw colormap
+                    mx.legend_colorbar(Mx, legendPos.x + 10, legendPos.y + (legendPos.height / 4),
+                        (legendPos.width / 4) - 10, legendPos.height / 2);
+                }
             }
             ix = ix + tw * 3;
             iy = iy + Mx.text_h * 0.3;

js/mx.js
@@ -4916,6 +4916,22 @@
     };
 
     /**
+     * @param Mx
+     * @param x
+     * @param y
+     * @param w
+     * @param h
+     * @private
+     */
+    mx.legend_colorbar = function(Mx, x, y, w, h) {
+        for (var j = 1; j < w; j++) {
+            var cidx = Math.floor(Mx.pixel.length * (j - 1) / w);
+            mx.draw_line(Mx, cidx, x + w - j, y, x + w - j, y + h);
+        }
+        mx.draw_box(Mx, Mx.fg, x + 0.5, y, w, h);
+    };
+
+    /**
      * Render image buffer to canvas.
      *
      * If we don't have access to Uint8ClampedArray (i.e. Firefox 3.6)

test/tests.js
@@ -2217,8 +2217,8 @@ interactiveTest('autoy with all zeros (pipe)', 'Does the autoscaling properly wo
     }, 500);
 });
 
-// By default, rasters have their autolevel set by the 
-// first 16 raster-lines.   
+// By default, rasters have their autolevel set by the
+// first 16 raster-lines.
 interactiveTest('t2000 file (default autol)', 'Is the plot red below the ~16th line?', function() {
     var container = document.getElementById('plot');
     var plot = new sigplot.Plot(container, {});
@@ -4056,7 +4056,7 @@ interactiveTest('slider', 'Do you see a sliders?', function() {
     // slidertag events happen whenever a slider is moved
     // programatically or by the user
     plot.addListener("slidertag", function(evt) {});
-    // sliderdrag events happen only when a slider is moved by 
+    // sliderdrag events happen only when a slider is moved by
     // the user
     plot.addListener("sliderdrag", function(evt) {});
 });
@@ -4284,6 +4284,11 @@ interactiveTest('p-cuts: turn on and off', 'Does the feature toggle with "p" key
     plot.overlay_href("dat/penny.prm");
 });
 interactiveTest('p-cuts: x-cut and y-cut without p-cuts display', 'Do the x and y plot display when clicked without the smaller plots?', function() {
+    var container = document.getElementById('plot');
+    var plot = new sigplot.Plot(container, {});
+    plot.overlay_href("dat/penny.prm");
+});
+interactiveTest('colorbar in legend', 'does the colorbar show in the legend?', function() {
     var container = document.getElementById('plot');
     var plot = new sigplot.Plot(container, {});
     plot.overlay_href("dat/penny.prm");

commit 805c03c44ec3aeacc683e53bf3b36adcac765115

fix: issue #55 so that legends with color bars render correctly.
This bug was introduced in ef66bc51347408191696c3daf683681d7e953485.

Basically there were three issues:
1) the code incorrectly used the header class instead of looking at the
layer type to determine if the line or color bar should be draw
2) the code didn't draw the color bar in the legend correctly due to a
typo, so it instead drew a blank box
3) the colorbar legend box was not scaled correctly if there was more
than one layer present.

js/sigplot.js
@@ -7281,8 +7281,8 @@
             legendPos.height);
 
         for (n = 0; n < Gx.lyr.length; n++) {
-            ix = xc + 4 * tw;
-            iy = yc + n * Mx.text_h + Mx.text_h; // additional text_h to
+            ix = Math.floor(xc + 4 * tw);
+            iy = Math.floor(yc + n * Mx.text_h + Mx.text_h); // additional text_h to
             // account for 0-based
             // indexing
             if (n === Gx.modlayer) {
@@ -7290,38 +7290,43 @@
                 // offset
             }
             if (Gx.lyr[n].display) {
-                ic = Gx.lyr[n].color;
-                if (Gx.lyr[n].line > 0) {
-                    thk = m.sign(Math.min(tw, Math.abs(Gx.lyr[n].thick)),
-                        Gx.lyr[n].thick);
-                    // added magic -3 offset to y coordinates to center lines
-                    // with text
-                    if (thk < 0 || thk === mx.L_dashed) {
-                        mx.draw_line(Mx, ic, ix - labelOffset, iy - 3, (ix + tw * 2) - labelOffset, iy - 3, Math.abs(thk), {
-                            mode: "dashed",
-                            on: 4,
-                            off: 4
-                        });
-                    } else {
-                        mx.draw_line(Mx, ic, ix - labelOffset, iy - 3, (ix + tw * 2) - labelOffset, iy - 3, Math.abs(thk));
+                // TODO refactor this code so that each layer type is responsible
+                // for drawing it's own legend icon, providing flexibliity down the road
+                // for other layer types
+                if (Gx.lyr[n] instanceof Layer1D) {
+                    ic = Gx.lyr[n].color;
+                    if (Gx.lyr[n].line > 0) {
+                        thk = m.sign(Math.min(tw, Math.abs(Gx.lyr[n].thick)),
+                            Gx.lyr[n].thick);
+                        // added magic -3 offset to y coordinates to center lines
+                        // with text
+                        if (thk < 0 || thk === mx.L_dashed) {
+                            mx.draw_line(Mx, ic, ix - labelOffset, iy - 3, (ix + tw * 2) - labelOffset, iy - 3, Math.abs(thk), {
+                                mode: "dashed",
+                                on: 4,
+                                off: 4
+                            });
+                        } else {
+                            mx.draw_line(Mx, ic, ix - labelOffset, iy - 3, (ix + tw * 2) - labelOffset, iy - 3, Math.abs(thk));
+                        }
                     }
-                }
-                if (Gx.lyr[n].symbol > 0) {
-                    // New logic here with 0.6*tw to help with legend symbol
-                    // sizing
-                    if (Gx.lyr[n].radius < 0) {
-                        thk = -m.trunc(0.6 * tw);
-                    } else {
-                        thk = Math.min(Gx.lyr[n].radius, m.trunc(0.6 * tw));
-                    }
+                    if (Gx.lyr[n].symbol > 0) {
+                        // New logic here with 0.6*tw to help with legend symbol
+                        // sizing
+                        if (Gx.lyr[n].radius < 0) {
+                            thk = -m.trunc(0.6 * tw);
+                        } else {
+                            thk = Math.min(Gx.lyr[n].radius, m.trunc(0.6 * tw));
+                        }
 
-                    mx.draw_symbol(Mx, ic, ix + tw - labelOffset, iy - 3,
-                        Gx.lyr[n].symbol, thk);
-                }
-                if (Gx.lyr[n].hcb["class"] === 2) {
-                    //draw colormap
-                    mx.legend_colorbar(Mx, legendPos.x + 10, legendPos.y + (legendPos.height / 4),
-                        (legendPos.width / 4) - 10, legendPos.height / 2);
+                        mx.draw_symbol(Mx, ic, ix + tw - labelOffset, iy - 3,
+                            Gx.lyr[n].symbol, thk);
+                    }
+                } else if (Gx.lyr[n] instanceof Layer2D) {
+                    //draw colorbar for 2D layers.  The bar needs to leave space for the modlayer
+                    // icon which occupies the first text-width
+                    mx.legend_colorbar(Mx, xc + (2 * tw) - labelOffset, iy - (Mx.text_h / 2),
+                        (tw * 4), (Mx.text_h / 2));
                 }
             }
             ix = ix + tw * 3;